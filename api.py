f1333f8804bd150744049ea0737800d05d04d5a9
SnorreEngh <122024096+SnorreEngh@users.noreply.github.com>
2023-02-11 15:34:15 +0100

    kan nå hente ut kurs, samt prøver å få dette på hovedsiden
    kan hente ut kursen til ethvert selskap . med dette printes også datoen og tiden dette er fra (noe som er maks 15 min etter sanntid om børsen er åpen). forbedringer i app.py

--- api.py
+++ api.py
@@ -1,8 +1,39 @@
 import requests
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selskap}&interval=5min&apikey=D5E2JVERV0RPWC30'
-r = requests.get(url)
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=D5E2JVERV0RPWC30'
+# apikey = D5E2JVERV0RPWC30
+r = requests.get(url, headers = {"User-agent": "Snorres mac"})
 data = r.json()
 
+print(data["Meta Data"]["2. Symbol"])
 
-print(data)
+# def IBM(selskap):
+#     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selskap}&interval=5min&apikey=demo'
+#     r = requests.get(url, headers = {"User-agent": "Snorres mac"})
+#     data = r.json()
+#     print(data["Meta Data"]["2. Symbol"])
+#     return r
+
+# def kurs(selskap):
+    # url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selskap}&interval=5min&apikey=demo'
+    # r = requests.get(url, headers = {"User-agent": "Snorres mac"})
+    # data = r.json()
+    # print(data["Time Series (5min)"]["2023-02-08 20:00:00"])
+    # return r
+
+
+
+def kurs(selskap):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selskap}&interval=5min&apikey=D5E2JVERV0RPWC30'
+    r = requests.get(url, headers = {"User-agent": "Snorres mac"})
+    data = r.json()
+
+    timeseries = data["Time Series (5min)"]
+    time_list_innhold = list(timeseries.values())
+    time_list_dato = list(timeseries)
+    time_list_dato[0]
+    print(time_list_dato[0], time_list_innhold[0]['1. open'])
+
+    return r
+
+kurs("IBM")
--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template
 import requests
+from api import kurs
 
 app = Flask(__name__)
 
+aksjeliste = kurs("IBM")
+
 @app.route("/")
 def hjem():
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={selskap}&apikey=demo"
-    
-    return render_template("index.html")
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={selskap}&interval=5min&apikey=D5E2JVERV0RPWC30"
+    r = requests.get(url, headers = {"User-agent": "Snorres mac"})
+    data = r.json()
+    print(data)
+
+    return render_template("index.html", aksjeliste=aksjeliste)
 
 @app.route("/selskaper")
 def selskaper():
-    return render_template("index.html", )
+    return render_template("selskaper.html")
+
+
+# timeseries = data["timeseries (5min)"]
+
+# time_list = list(timeseries)
+# time_list[0]
+# print(time_list)
+
+
+# timeseries = {
+#     "123": "hei"
+#     "123": "hei"
+#     "123": "hei"
+#     "123": "hei"
+# }
+
+# time_list = list(timeseries.values())
